9a3082c64a24fd88a652d1002229e1f959ae3c61
smith64fx <michael@schmidigital.de>
2016-04-15 03:28:22 +0200

    basic config loaded, loading hooks now

--- bin/ship-start.js
+++ bin/ship-start.js
@@ -1,16 +1,18 @@
 var nodepath = require('path'),
-	package = require('../package.json')
+	package = require('../package.json'),
+	rconf = require('../lib/app/configuration/rc'),
 	Ship = require('../lib/app');
 
 module.exports = function () {
  
-	// Build initial scope
-	var scope = {
+	
+	// Build initial scope, mixing-in rc config
+	var scope = _.merge({
 		rootPath: process.cwd(),
-		shipRoot: nodepath.resolve(__dirname, '..'),
-		shipPackageJSON: package
-	};
-
+		//shipRoot: nodepath.resolve(__dirname, '..'),
+		sailsPackageJSON: package
+	}, rconf);
+	
 
 	// Pass the original CLI arguments down to the generator
 	// (but first, remove commander's extra argument)
@@ -19,6 +21,15 @@ module.exports = function () {
 
 
 	var globalShip = Ship();
-	globalShip.start(scope);
+	globalShip.start(scope, afterwards);
+
+
+	function afterwards (err, sails) {
+		if (err) {
+		var message = err.stack ? err.stack : err;
+		sails ? console.log(message) : console.log(message); process.exit(1);
+		}
+		// try {console.timeEnd('cli_lift');}catch(e){}
+	}
 
 }
